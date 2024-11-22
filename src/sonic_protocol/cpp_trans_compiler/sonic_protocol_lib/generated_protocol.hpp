6278351ae0f862b5644b91a7e05e38e015d59ad7
David Wild <david.wild2002@gmail.com>
2024-11-22 16:04:52 +0100

    changed to constevel

--- src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/generated_protocol.hpp
+++ src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/generated_protocol.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <initializer_list>
 #include <array>
+#include <cstdint>
 
 #include "sonic_protocol_lib/protocol.hpp"
 
@@ -9,14 +10,14 @@ namespace sonic_protocol_lib {
 #define FIELD_LIMITS
 /**/FIELD_LIMITS/**/ // the python script will replace this
 
-#define PROTOCOL_COUNT {0}
-constexpr std::size_t protocol_count() {
+#define PROTOCOL_COUNT 0
+consteval std::size_t protocol_count() {
     return /**/PROTOCOL_COUNT/**/; // the python script will replace this
 }
 #undef PROTOCOL_COUNT
 
 #define PROTOCOLS {}
-constexpr std::array<Protocol, protocol_count()> protocols() {
+consteval std::array<Protocol, protocol_count()> protocols() {
     return /**/PROTOCOLS/**/; // the python script will replace this
 }
 #undef PROTOCOLS
