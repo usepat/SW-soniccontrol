60caca0f4a419d0af1a9f2fd60a247386f0e81ac
SonicTS <thomas.schoeller@usepat.com>
2024-11-22 15:40:19 +0100

    .

--- src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/include/sonic_protocol_lib/protocol.hpp
+++ src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/include/sonic_protocol_lib/protocol.hpp
@@ -6,10 +6,9 @@
 
 namespace sonic_protocol_lib {
 
-template <std::size_t CommandCount, std::size_t AnswerCount>
 struct Protocol {
-    std::array<CommandDef, CommandCount> commands;
-    std::array<AnswerDef, AnswerCount> answers;
+    std::initializer_list<CommandDef> commands;
+    std::initializer_list<AnswerDef> answers;
 };
 
 }
