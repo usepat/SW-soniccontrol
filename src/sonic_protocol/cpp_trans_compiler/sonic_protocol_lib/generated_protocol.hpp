47b57980783710c54d27043dc3e9a94da2e475be
SonicTS <thomas.schoeller@usepat.com>
2024-11-25 14:22:07 +0100

    .

--- src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/generated_protocol.hpp
+++ src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/generated_protocol.hpp
@@ -18,7 +18,7 @@ consteval std::size_t protocol_count() {
 #undef PROTOCOL_COUNT
 
 #define PROTOCOLS {}
-consteval std::array<IProtocol, protocol_count()> protocols() {
+consteval std::array<IProtocol*, protocol_count()> protocols() {
     /**/PROTOCOLS/**/; // the python script will replace this
 }
 #undef PROTOCOLS
