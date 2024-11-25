e4b818c407af3afcb57117e5faeed7ef6d6c3969
SonicTS <thomas.schoeller@usepat.com>
2024-11-25 14:38:25 +0100

    .

--- src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/include/sonic_protocol_lib/protocol.hpp
+++ src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/include/sonic_protocol_lib/protocol.hpp
@@ -26,45 +26,41 @@ public:
     virtual std::span<const AnswerDef> getAnswersSpan() const = 0;
 };
 
-template<size_t CommandCount, size_t AnswerCount>
-class ProtocolTemplated : public IProtocol {
-private:
-  Version version;
-  DeviceType device;
-  bool isRelease;
-  std::string_view options;
-  etl::array<CommandDef, CommandCount> commands;
-  etl::array<AnswerDef, AnswerCount> answers;
-public:
-  constexpr ProtocolTemplated(Version ver, DeviceType dev, bool release, std::string_view opts, 
-                const etl::array<CommandDef, CommandCount>& cmds, 
-                const etl::array<AnswerDef, AnswerCount>& ans)
-    : version(ver), device(dev), isRelease(release), options(opts), commands(cmds), answers(ans) {}
+template <size_t CommandCount, size_t AnswerCount>
+struct ProtocolTemplated : public IProtocol {
+    Version version;
+    DeviceType device;
+    bool isRelease;
+    std::string_view options;
+    etl::array<CommandDef, CommandCount> commands;
+    etl::array<AnswerDef, AnswerCount> answers;
 
-  Version getVersion() const override {
-    return version;
-  }
+    // Override virtual functions for runtime use
+    Version getVersion() const override {
+        return version;
+    }
 
-  DeviceType getDevice() const override {
-    return device;
-  }
+    DeviceType getDevice() const override {
+        return device;
+    }
 
-  bool getIsRelease() const override {
-    return isRelease;
-  }
+    bool getIsRelease() const override {
+        return isRelease;
+    }
 
-  std::string_view getOptions() const override {
-    return options;
-  }
+    std::string_view getOptions() const override {
+        return options;
+    }
 
-  std::span<const CommandDef> getCommandsSpan() const override {
-    return std::span<const CommandDef>(commands.data(), commands.size());
-  }
+    std::span<const CommandDef> getCommandsSpan() const override {
+        return std::span<const CommandDef>(commands.data(), commands.size());
+    }
 
-  std::span<const AnswerDef> getAnswersSpan() const override {
-    return std::span<const AnswerDef>(answers.data(), answers.size());
-  }
+    std::span<const AnswerDef> getAnswersSpan() const override {
+        return std::span<const AnswerDef>(answers.data(), answers.size());
+    }
 };
+
 
 template<size_t Count>
 using Protocol = ProtocolTemplated<Count, Count>;
