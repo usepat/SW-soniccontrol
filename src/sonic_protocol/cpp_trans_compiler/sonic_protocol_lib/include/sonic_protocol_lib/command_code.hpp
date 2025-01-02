c8a8f5f800f93e5fa2e55094e53974873dc8e5b1
David Wild <a12134916@unet.univie.ac.at>
2025-01-02 11:31:52 +0100

    inline keyword needed for linking, else multiple definitions

--- src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/include/sonic_protocol_lib/command_code.hpp
+++ src/sonic_protocol/cpp_trans_compiler/sonic_protocol_lib/include/sonic_protocol_lib/command_code.hpp
@@ -11,7 +11,7 @@ enum class CommandCode : int16_t  {
 
 
 #define COMMAND_CODE_SWITCH_CASE
-bool isValidCode(std::uint16_t value) {
+inline bool isValidCode(std::uint16_t value) {
     CommandCode code = static_cast<CommandCode>(value);
     switch (code) {
         /**/COMMAND_CODE_SWITCH_CASE/**/
