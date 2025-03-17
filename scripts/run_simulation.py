943ff76830ffe38f2ee020329b68e156ca56f4ee
SonicTS <thomas.schoeller@usepat.com>
2025-03-17 10:44:01 +0100

    commented out line, that overwrites simulation_exe_path

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -8,6 +8,6 @@ from soniccontrol_gui.utils.widget_registry import WidgetRegistry
 if __name__ == "__main__":
     WidgetRegistry.set_up()
     simulation_exe_path = Path(os.environ["SIMULATION_EXE_PATH"])
-    simulation_exe_path = Path("/home/usepat/GitHub/FW-sonic-firmware/build/linux/mvp_simulation_descale/test/cli/cli_simulation_descale/cli_simulation_descale")
+    #simulation_exe_path = Path("/home/usepat/GitHub/FW-sonic-firmware/build/linux/mvp_simulation_descale/test/cli/cli_simulation_descale/cli_simulation_descale")
     start_gui(simulation_exe_path)
     asyncio.run(WidgetRegistry.clean_up())
