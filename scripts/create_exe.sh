d99fec58b87002538b969c80c0272d0b9fe76843
SonicTS <thomas.schoeller@usepat.com>
2025-05-19 10:15:50 +0200

    changed exe scripts

--- scripts/create_exe.sh
+++ scripts/create_exe.sh
@@ -7,5 +7,5 @@ pyinstaller --noconfirm --onedir --windowed \
 --name "SonicControl" \
 --collect-all soniccontrol_gui \
 --collect-all soniccontrol \
---add-data "sonic_script_examples:sonic_script_examples"
+--add-data "sonic_script_examples:sonic_script_examples" \
 "${WORKSPACE_DIR}/src/soniccontrol_gui/__main__.py"
