0c8d999f6b7726311cede8a60d3bfe58926df4b4
David Wild <a12134916@unet.univie.ac.at>
2025-07-18 16:18:49 +0200

    removed InputSource enum from protocol v2.0.0 because of changes in the firmware

--- src/sonic_protocol/protocols/protocol_v2_0_0/protocol_v2_0_0.py
+++ src/sonic_protocol/protocols/protocol_v2_0_0/protocol_v2_0_0.py
@@ -39,6 +39,10 @@ class Protocol_v2_0_0(ProtocolList):
     def custom_data_types(self) -> Dict[str, type]:
         data_types = {}
         data_types.update(self._previous_protocol.custom_data_types)
+
+        # We remove Input source, because we refactored it in the firmware into ControlMode and CommunicationChannel
+        data_types.pop("E_INPUT_SOURCE")
+        
         return data_types
 
     def supports_device_type(self, device_type: DeviceType) -> bool:
@@ -48,7 +52,15 @@ class Protocol_v2_0_0(ProtocolList):
         command_contract_list: List[CommandContract] = [clear_errors, restart_device]
         if protocol_type.device_type == DeviceType.DESCALE:
             command_contract_list.extend([get_adc])
-        return { command_contract.code: command_contract for command_contract in command_contract_list } 
-    
+
+        command_contract_dict: Dict[ICommandCode, CommandContract | None] = {
+            command_contract.code: command_contract for command_contract in command_contract_list 
+        } 
+
+        # setting the input source should be done in the configurator and not operator
+        command_contract_dict[CommandCode.SET_INPUT_SOURCE] = None
+
+        return command_contract_dict
+
     def _get_device_constants_for(self, protocol_type: ProtocolType) -> Dict[DeviceParamConstantType, Any]:
         return {}
