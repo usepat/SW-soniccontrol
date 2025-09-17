575a68856963305f1cf915f02d103b95c0ea9676
SonicTS <thomas.schoeller@usepat.com>
2025-09-17 17:14:23 +0200

    Reverted .lower()

--- src/soniccontrol_gui/utils/plotlib/__main__.py
+++ src/soniccontrol_gui/utils/plotlib/__main__.py
@@ -1,4 +1,5 @@
 import matplotlib.figure
+from sonic_protocol.field_names import EFieldName
 from soniccontrol_gui.views.measure.plotting import PlotView, PlotViewModel
 from soniccontrol_gui.utils.plotlib.plot import Plot
 from soniccontrol_gui.utils.plotlib.plot_builder import PlotBuilder 
@@ -16,7 +17,7 @@ def main():
 
     filepath = "./logs/status_log.csv"
     data = pd.read_csv(filepath)
-    data["TIMESTAMP"] = pd.to_datetime(data["TIMESTAMP"])
+    data[EFieldName.TIMESTAMP.name.lower()] = pd.to_datetime(data[EFieldName.TIMESTAMP.name.lower()])
     plot.update_data(data)
 
     root = tk.Tk()
