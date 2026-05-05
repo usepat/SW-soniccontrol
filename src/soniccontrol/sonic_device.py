9acf7ed23b90851abd8d93f279b2db9f236236ec
David Wild <a12134916@unet.univie.ac.at>
2026-05-05 15:19:17 +0200

    removed redundant classes

--- src/soniccontrol/sonic_device.py
+++ src/soniccontrol/sonic_device.py
@@ -261,11 +261,11 @@ class SonicDevice:
     async def restart(self):
         try:
             await self.execute_command(commands.RestartDevice()) 
-        except (ConnectionError, ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError):
+        except ConnectionError:
             pass # could throw a connection error, device may not respond anymore, because it is restarting
         
         try:
             await self.disconnect()
-        except (ConnectionError, ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError):
+        except ConnectionError:
             pass # could throw a connection error, device may not respond anymore, because it is restarting
         
