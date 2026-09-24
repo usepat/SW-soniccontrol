5ef2b7ffb18a4ddbcc2837a8fecd9b9b87fa1321
David Wild <a12134916@unet.univie.ac.at>
2026-09-24 14:02:58 +0200

    colorized the logs with tags in tkinter

--- src/soniccontrol_gui/views/control/logging.py
+++ src/soniccontrol_gui/views/control/logging.py
@@ -1,6 +1,9 @@
 
+from enum import Enum
 import logging
-from typing import Callable, Dict
+from typing import Callable, Dict, List, Tuple
+
+import attrs
 from soniccontrol.logger.logger_discovery import DeviceLoggerDiscovery, PythonLoggerDiscovery
 from soniccontrol.sonic_device import SonicDevice
 from soniccontrol_gui import constants
@@ -16,9 +19,9 @@ from soniccontrol.utils.events import Event
 from soniccontrol_gui.resources import images
 from soniccontrol_gui.utils.image_loader import ImageLoader
 from soniccontrol_gui.utils.observable_list import ObservableList
-from soniccontrol_gui.widgets.xyscrolled_frame import XYScrolledFrame
 from soniccontrol_gui.widgets.message_box import MessageBox
 from soniccontrol_gui.widgets.notebook import Notebook
+
 
 
 class Logging(UIComponent):
@@ -96,6 +99,19 @@ class LoggingView(TabView):
         self._open_logs_button.configure(command=command)
 
 
+class TagType(Enum):
+    TIMESTAMP = "timestamp"
+    LOGLEVEL = "loglevel"
+    LOGGER = "logger"
+
+@attrs.define()
+class Tag:
+    tag_type: TagType
+    begin_line: int
+    begin_col: int
+    end_line: int
+    end_col: int
+
 
 class LoggingTab(UIComponent):
     def __init__(self, parent: UIComponent, logs: ObservableList):
@@ -108,14 +124,40 @@ class LoggingTab(UIComponent):
 
     def _init_logs(self):
         for log in self._logs:
-            self._view.append_text_line(log)
-
-    def _add_log(self, e: Event):
-        self._view.append_text_line(e.data["item"])
+            self._add_log_text(log)
         self._view.scroll_to_end()
 
+    def _add_log(self, e: Event):
+        self._add_log_text(e.data["item"])
+        self._view.scroll_to_end()
+       
+    def _add_log_text(self, log_text: str):
+        # insert text and get the start index of the inserted text in the Text widget
+        start_line = self._view.append_text_line(log_text)
+
+        # 2026-09-24 10:04:37,236: INFO - simulation.SerialCommunicator -
+        timestamp_sep = ": "
+        sep = " -"
+        end_timestamp = log_text.index(timestamp_sep)
+        begin_loglevel = end_timestamp + len(timestamp_sep)
+        end_loglevel = log_text.index(sep, begin_loglevel)
+        begin_logger_name = end_loglevel + len(sep)
+        end_logger_name = log_text.index(sep, begin_logger_name)
+
+        tags = [
+            Tag(TagType.TIMESTAMP, start_line, 0, start_line, end_timestamp),
+            Tag(TagType.LOGLEVEL, start_line, begin_loglevel, start_line, end_loglevel),
+            Tag(TagType.LOGGER, start_line, begin_logger_name, start_line, end_logger_name),
+        ]
+        for tag in tags:
+            self._view.add_tag(tag)
+
     def _remove_log(self, e: Event):
-        self._view.destroy_ith_text_line(0)
+        log_text: str = e.data["item"]
+        num_lines = log_text.count("\n") + 1
+        # pop num_lines from beginning
+        for _ in range(num_lines):
+            self._view.destroy_ith_text_line(0)
 
 class LoggingTabView(TabView):
     def __init__(self, master: ttk.Window, *args, **kwargs) -> None:
@@ -129,6 +171,12 @@ class LoggingTabView(TabView):
     def tab_title(self) -> str:
         return ui_labels.LOGS_LABEL
 
+    @property
+    def num_log_lines(self) -> int:
+        result = self._text_widget.count("1.0", ttk.END, "lines")
+        assert result is not None
+        return result[0]
+
     def _initialize_children(self) -> None:
         self._main_frame: ttk.Frame = ttk.Frame(self)
         self._output_frame: ttk.Labelframe = ttk.Labelframe(
@@ -139,6 +187,9 @@ class LoggingTabView(TabView):
             self._output_frame, 
             font=("Consolas", 10)
         )
+        self._text_widget.tag_configure(TagType.TIMESTAMP.value, foreground="blue")
+        self._text_widget.tag_configure(TagType.LOGLEVEL.value, foreground="blue")
+        self._text_widget.tag_configure(TagType.LOGGER.value, foreground="blue")
         self._scrollbar_y = ttk.Scrollbar(self._output_frame, orient="vertical", command=self._text_widget.yview)
         self._scrollbar_x = ttk.Scrollbar(self._output_frame, orient="horizontal", command=self._text_widget.xview)
         
@@ -173,7 +224,17 @@ class LoggingTabView(TabView):
 
     def append_text_line(self, text: str):
         self._text_widget.configure(state=ttk.NORMAL)
+        start_index = self._text_widget.index("end-1c")
         self._text_widget.insert(ttk.END, text + "\n")
+        self._text_widget.configure(state=ttk.DISABLED)
+        return int(start_index.split(".")[0])
+
+    def add_tag(self, tag: Tag):
+        begin = f"{tag.begin_line}.{tag.begin_col}"
+        end = f"{tag.end_line}.{tag.end_col}"
+
+        self._text_widget.configure(state=ttk.NORMAL)
+        self._text_widget.tag_add(tag.tag_type.value, begin, end)
         self._text_widget.configure(state=ttk.DISABLED)
         
     def scroll_to_end(self):
